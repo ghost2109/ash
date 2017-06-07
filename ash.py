3b400a2372fed1c976853608e9e92697a3737b2b
James Phillips <ghost2109@gmail.com>
2017-06-07 10:39:11 +0100

    Add files via upload

--- ash.py
+++ ash.py
@@ -66,7 +66,7 @@ class AwsConsole(Cmd):
     def __init__(self):
 
         # ASH version number
-        self.version        = '1.0.14'
+        self.version        = '1.0.15'
 
         if '--upgrade' in sys.argv:
             v = check_output(['git', 'ls-remote', '--tags', 'https://github.com/ghost2109/ash'])
@@ -323,7 +323,6 @@ class AwsConsole(Cmd):
                       for grp in sgid:
 
                         if '-rds-' in grp['GroupName']:
-                          print(grp['GroupName'])
                           check = grp['GroupId']
                         else:
                           check = ''
